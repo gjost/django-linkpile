52b6f3a1615c4f5f40d8b262be485371f7f6f16d
geoffrey jost <geoffrey@jostwebwerks.com>
2014-06-18 08:40:34 -0700

    initial commit
    Thx pydanny! https://github.com/pydanny/cookiecutter-djangopackage

---
+++ tests/test_models.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+test_linkpile
+------------
+
+Tests for `linkpile` models module.
+"""
+
+import os
+import shutil
+import unittest
+
+from linkpile import models
+
+
+class TestLinkpile(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_something(self):
+        pass
+
+    def tearDown(self):
+        pass
