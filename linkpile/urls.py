bf98a7367508f88915e65a5680a88a5b81315c2f
geoffrey jost <geoffrey@jostwebwerks.com>
2016-05-01 18:30:37 -0700

    update urls.py for newer Django version

--- linkpile/urls.py
+++ linkpile/urls.py
@@ -1,15 +1,15 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from django.views.generic import TemplateView
 
 from linkpile.feeds import LinksFeed
+from linkpile import views
 
-urlpatterns = patterns(
-    '',
+urlpatterns = [
     url(r'^feed/$', LinksFeed()),
-    url(r'^export/$', 'linkpile.views.export', name='linkpile-export'),
-    url(r'^new/$', 'linkpile.views.new', name='linkpile-new'),
-    url(r'^link/(?P<link_id>\d+)/edit/$', 'linkpile.views.edit', name='linkpile-edit'),
-    url(r'^link/(?P<link_id>\d+)/$', 'linkpile.views.detail', name='linkpile-link'),
-    url(r'^(?P<tags>[\w:$&-_-+/.]+)/$', 'linkpile.views.tags', name='linkpile-tags'),
-    url(r'^$', 'linkpile.views.index', name='linkpile-index'),
-)
+    url(r'^export/$', views.export, name='linkpile-export'),
+    url(r'^new/$', views.new, name='linkpile-new'),
+    url(r'^link/(?P<link_id>\d+)/edit/$', views.edit, name='linkpile-edit'),
+    url(r'^link/(?P<link_id>\d+)/$', views.detail, name='linkpile-link'),
+    url(r'^(?P<tags>[\w:$&-_-+/.]+)/$', views.tags, name='linkpile-tags'),
+    url(r'^$', views.index, name='linkpile-index'),
+]
